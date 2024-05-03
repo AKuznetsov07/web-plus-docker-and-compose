04b2763bc65c8681a058d033182bc5f333e9c03b
AKuznetsov <artemkuznetsov07@gmail.com>
2024-05-04 00:36:35 +0300

    small fixes

--- backend/src/common/jwtRegisterConfig.ts
+++ backend/src/common/jwtRegisterConfig.ts
@@ -2,7 +2,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 export const jwtConfig = {
   imports: [ConfigModule],
   useFactory: async (configService: ConfigService) => ({
-    secret: configService.get<string>('jwt_secret'),
+      secret: configService.get<string>('jwtSecret'),
   }),
   inject: [ConfigService],
 };
